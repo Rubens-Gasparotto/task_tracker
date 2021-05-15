64c6865a425f50b32f413dcf03202d57292e11a9
Rubens Gasparotto <rubens.gasparotto@mail.tantto.com.br>
2021-05-15 18:38:40 -0300

    commit inicial

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation()
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+
+      {location.pathname === '/' && <Button text={showAddTask ? 'Close' : 'Add'} color={showAddTask ? 'red' : 'green'} onClick={onAdd} />}
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default Header
